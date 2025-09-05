0df55d0ebdf6fd4bf3f45fdeca51bf5e5d45c38b
MoussaMohammed-code <moussamohammed2005@gmail.com>
2025-09-05 19:33:09 +0100

    Update challenge 1 and 2

--- Day03/Struct/challenge01.c
+++ Day03/Struct/challenge01.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-typedef struct personne{
+struct personne{
     char nom[20];
     char prenom[20];
     int age;
 } personne;
 
 int main(){
-    personne p1={"Moussa","Mohammed",20};
+    struct personne p1={"Moussa","Mohammed",20};
     printf("Nom : %s\n",p1.nom);
-    printf("Nom : %s\n",p1.prenom);
-    printf("Nom : %d\n",p1.age);
+    printf("Prenom : %s\n",p1.prenom);
+    printf("Age : %d\n",p1.age);
     return 0;
 }
